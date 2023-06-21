a25ff0f8a7fb7dd222189d9dc7f70e95a895b3d3
iHotaru <124116543+iHotaru@users.noreply.github.com>
2023-06-21 22:34:52 +0000

    Done lol

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Enter a word:");
-    
-    //Your code will return the length of the entered String
+
+    Scanner x = new Scanner(System.in);
+    String y = x.next();
+
+    System.out.println(y.length());
   }
 }
